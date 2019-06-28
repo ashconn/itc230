4d1a78382db023c36467fa9973ca18c538d5e5ff
Ashley <ashconn@live.com>
2019-06-27 18:03:44 -0700

    Homework 1

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+
+http.createServer((req, res) => {
+    const path = req.url.toLowerCase();
+    switch (path) {
+        case '/':
+            const fs = require("fs");
+            fs.readFile("public/home.html", (err, data) => {
+                if (err) return console.error(err);
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data.toString());
+            });
+            break;
+        case '/about':
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('This is my app for ITC 230.');
+            break;
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Not found');
+            break;
+    }
+}).listen(process.env.PORT || 3000);
