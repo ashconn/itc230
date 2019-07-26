62149ca7e4f9633d6abfd639a81f9eae66173c78
ashconn <46731484+ashconn@users.noreply.github.com>
2019-07-25 20:21:32 -0700

    Homework 6

--- games.js
+++ games.js
@@ -1,15 +1,18 @@
 'use strict'
 
-let games = [
-    { title: 'Dragon Age: Inquistion', genre: 'RPG', year: 2014 },
-    { title: 'Pokemon: Crystal', genre: 'RPG', year: 2000 },
-    { title: 'Animal Crossing', genre: 'Simulation', year: 2001 },
-    { title: 'Final Fantasy: VII', genre: 'J-RPG', year: 1997 },
-    { title: 'Batman: Arkham City', genre: 'Action-Adventure', year: 2011 }
-];
+const Game = require("./models/games");
 
 exports.getAll = () => {
-    return games;
+    console.log('getall');
+    return Game.find({}, (err, result) => {
+        console.log(err);
+        console.log(result);
+        if (err) {
+            console.log(err);
+        } else {
+            return result;
+        };
+    });
 };
 
 exports.get = (title) => {
@@ -24,4 +27,13 @@ exports.delete = (title) => {
         return item.title !== title;
     });
     return { deleted: oLength !== games.length, total: games.length };
+};
+
+exports.add = (newGame) => {
+    const oLength = games.length;
+    const found = this.get(newGame.title);
+    if (!found) {
+        games.push(newGame);
+    }
+    return {added: oLength !== games.length, total: games.length };
 };
--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 'use strict'
 
-let games = require("./games.js");
-
+const games = require("./games");
+const Game = require("./models/games");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
+//Express settings
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public')); // set location for static files
 app.use(bodyParser.urlencoded({extended: true})); // parse form submissions
+app.use('/api', require("cors")());
+app.use((err, req, res, next) => {
+    console.log(err);
+});
 
+//Handlebars settings
 const handlebars =  require("express-handlebars");
 app.engine(".html", handlebars({extname: '.html', defaultLayout: false}));
 app.set("view engine", ".html");
 
 // send static file as response
 app.get('/', (req, res) => {
-    res.render('home', {games: games.getAll()}); 
+    games.getAll().then((games) => {
+        res.render('home', {games: games});
+    }).catch((err) => {
+        return next(err);
+    });
 });
 
 // send plain text response
@@ -25,31 +35,81 @@ app.get('/about', (req, res) => {
     res.send('About page.');
 });
 
-// handle get
+// handle get methods
+
 app.get('/delete', (req, res) => {
-    let result = games.delete(req.query.title);
-    res.render('delete', {
-        title: req.query.title, result: result
+    Game.remove({title: req.query.title}, (err, result) => {
+        if (err) return next(err);
+        const deleted = result.result !==0;
+    Game.count((err, total) => {
+        res.type('text/html');
+        res.render('delete', {title: req.query.title, deleted: result.result !==0, total: total });
+        });
     });
 });
 
-app.get('/detail', (req, res) => {
-    console.log(req.query)
-    var found = games.get(req.query.title);
-    res.render('detail', {
-        title: req.query.title, 
-        result: found
+app.get('/detail', (req, res, next) => {
+    Game.findOne({title: req.query.title}, (err, games) => {
+        if (err) return next(err);
+        res.type('text/html');
+        res.render('detail', {result: games});
     });
 });
 
 // handle post
-app.post('/detail', (req, res) => {
-    console.log(req.body)
-    var found = games.get(req.body.title);
-    res.render('detail', {
-        title: req.body.title, 
-        result: found, 
-        games: games.getAll()
+app.post('/detail', (req, res, next) => {
+    Game.findOne({title: req.body.title}, (err, games) => {
+        if (err) return next(err);
+        res.type('text/html');
+        res.render('detail', {result: games});
+    });
+});
+
+// APIs
+app.get('/api/add/:title/:genre/:year', (req, res, next) => {
+    const title = req.params.title;
+    Game.updateOne({title: title}, {title: title, genre: req.params.genre, year: req.params.year}, {upsert: true}, (err, result) => {
+        if (err) return next(err);
+        res.json({updated: result.n});
+    });
+});
+
+app.post('/api/add/', (req, res, next) => {
+    if (!req.body._id) {
+        const game = new Game({title: req.body.title, genre: req.body.genre, year: req.body.year});
+        game.save((err, newGame) => {
+            if (err) return next(err);
+            console.log(newGame);
+            res.json({updated: 0, _id: newGame._id});
+        });
+    } else {
+        Game.updateOne({_id: req.body._id}, {title: req.body.title, genre: req.body.genre, year: req.body.year}, (err, result) => {
+            if (err) return next(err);
+            res.json({updated: result.n});
+        });
+    };
+});
+
+app.get('api/game', (req, res, next) => {
+    Game.find((err, result) => {
+        if (err || !result) return next(err);
+        res.json(result);
+    });
+});
+
+app.get('/api/game/:title', (req, res, next) => {
+    const title = req.params.title;
+    console.log(title);
+    Game.findOne({title: title}, (err, result) => {
+        if (err || !result) return next(err);
+        res.json(result);
+    });
+});
+
+app.get('/api/delete/:title', (req, res, next) => {
+    Game.remove({"title": req.params.title}, (err, result) => {
+        if (err) return next(err);
+        res.json({"deleted": result.result});
     });
 });
 
