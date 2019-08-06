9bf23feff62b277670f5e35697489f2904b40d27
ashconn <46731484+ashconn@users.noreply.github.com>
2019-08-06 16:16:12 -0700

    HW 4

--- games.js
+++ games.js
@@ -2,6 +2,14 @@
 
 const Game = require("./models/games");
 
+let games = [
+    { title: 'Dragon Age: Inquistion', genre: 'RPG', year: 2014 },
+    { title: 'Pokemon: Crystal', genre: 'RPG', year: 2000 },
+    { title: 'Animal Crossing', genre: 'Simulation', year: 2001 },
+    { title: 'Final Fantasy: VII', genre: 'J-RPG', year: 1997 },
+    { title: 'Batman: Arkham City', genre: 'Action-Adventure', year: 2011 }
+];
+
 exports.getAll = () => {
     console.log('getall');
     return Game.find({}, (err, result) => {
